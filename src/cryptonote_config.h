55485e569f799971d1c07ff64766b9f676791fc3
shopglobal <president@worldvaporexpo.com>
2017-12-17 01:41:13 -0500

    safe

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -155,9 +155,9 @@ namespace config
   uint16_t const RPC_DEFAULT_PORT = 18091;
   uint16_t const ZMQ_RPC_DEFAULT_PORT = 18092;
   boost::uuids::uuid const NETWORK_ID = { {
-      0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
+      0x3d, 0x8, 0x14, 0x4f , 0xe3, 0x49 , 0x19, 0x2b, 0x31, 0x8, 0x2b, 0x31, 0x8, 0x8, 0x19, 0xe3
     } }; // Bender's nightmare
-  std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
+  std::string const GENESIS_TX = "013201ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101465fe22ec785363d26574087d8be3f8aa3aad34da8172bce368e679b41b7a45e";
   uint32_t const GENESIS_NONCE = 10000;
 
   namespace testnet
