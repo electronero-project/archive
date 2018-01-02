b4eea21539820b67b0104db61ea95fc9e7e07b9c
Mark Allen Evans <president@worldvaporexpo.com>
2018-01-02 05:57:49 -0500

    Update command_server.cpp

--- src/daemon/command_server.cpp
+++ src/daemon/command_server.cpp
@@ -1,3 +1,4 @@
+// Copyright (c) 2014-2017, The Mynt Project
 // Copyright (c) 2014-2017, The Monero Project
 // 
 // All rights reserved.
@@ -321,7 +322,7 @@ bool t_command_server::help(const std::vector<std::string>& args)
 std::string t_command_server::get_commands_str()
 {
   std::stringstream ss;
-  ss << "Monero '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << std::endl;
+  ss << "Mynt '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << std::endl;
   ss << "Commands: " << std::endl;
   std::string usage = m_command_lookup.get_usage();
   boost::replace_all(usage, "\n", "\n  ");
