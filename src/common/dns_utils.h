9b6c956c7c63edab780ece726b37c8e2083e7df6
Mark Allen Evans <president@worldvaporexpo.com>
2018-01-03 18:41:05 -0500

    Update dns_utils.h

--- src/common/dns_utils.h
+++ src/common/dns_utils.h
@@ -1,3 +1,4 @@
+// Copyright (c) 2017-2018, The Mynt Project
 // Copyright (c) 2014-2017, The Monero Project
 // 
 // All rights reserved.
@@ -108,7 +109,7 @@ public:
    * @brief Gets a DNS address from OpenAlias format
    *
    * If the address looks good, but contains one @ symbol, replace that with a .
-   * e.g. donate@getmonero.org becomes donate.getmonero.org
+   * e.g. donate@myntnote.org becomes donate.myntnote.org
    *
    * @param oa_addr  OpenAlias address
    *
