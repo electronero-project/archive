e96b4bc65ddc41cc0198b64d1d173a656943fe2c
Mark Allen Evans <president@worldvaporexpo.com>
2018-01-03 22:24:08 -0500

    Update open_wallet_4.sh

--- tests/libwallet_api_tests/scripts/open_wallet_4.sh
+++ tests/libwallet_api_tests/scripts/open_wallet_4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rlwrap mynt-wallet-cli --wallet-file wallet_04.bin --password "" --testnet --trusted-daemon --daemon-address localhost:21090  --log-file wallet_04.log
+rlwrap mynt-wallet-cli --wallet-file wallet_04.bin --password "" --testnet --trusted-daemon --daemon-address localhost:24090  --log-file wallet_04.log
 
