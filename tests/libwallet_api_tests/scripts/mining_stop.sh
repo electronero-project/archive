ed8e98c73d348cc72ee4bef8197b1809a3ef3c6b
Mark Allen Evans <president@worldvaporexpo.com>
2018-01-03 22:24:49 -0500

    Update mining_stop.sh

--- tests/libwallet_api_tests/scripts/mining_stop.sh
+++ tests/libwallet_api_tests/scripts/mining_stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rlwrap mynt-wallet-cli --wallet-file wallet_m --password "" --testnet --trusted-daemon --daemon-address localhost:21090  --log-file wallet_miner.log stop_mining
+rlwrap mynt-wallet-cli --wallet-file wallet_m --password "" --testnet --trusted-daemon --daemon-address localhost:24090  --log-file wallet_miner.log stop_mining
 
