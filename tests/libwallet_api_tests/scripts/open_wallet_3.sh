be3f27d80acb2b1b70cc730a2023b25559122ca5
Mark Allen Evans <president@worldvaporexpo.com>
2018-01-03 22:24:18 -0500

    Update open_wallet_3.sh

--- tests/libwallet_api_tests/scripts/open_wallet_3.sh
+++ tests/libwallet_api_tests/scripts/open_wallet_3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rlwrap mynt-wallet-cli --wallet-file wallet_03.bin --password "" --testnet --trusted-daemon --daemon-address localhost:21090  --log-file wallet_03.log
+rlwrap mynt-wallet-cli --wallet-file wallet_03.bin --password "" --testnet --trusted-daemon --daemon-address localhost:24090  --log-file wallet_03.log
 
