a2173b408260d3e8c87e092ac0070ec69e2c3801
Mark Allen Evans <president@worldvaporexpo.com>
2018-01-03 22:23:45 -0500

    Update create_wallets.sh

--- tests/libwallet_api_tests/scripts/create_wallets.sh
+++ tests/libwallet_api_tests/scripts/create_wallets.sh
@@ -2,7 +2,7 @@
 
 function create_wallet {
     wallet_name=$1
-    echo 0 | mynt-wallet-cli  --testnet --trusted-daemon --daemon-address localhost:21090 --generate-new-wallet $wallet_name --password "" --restore-height=1
+    echo 0 | mynt-wallet-cli  --testnet --trusted-daemon --daemon-address localhost:24090 --generate-new-wallet $wallet_name --password "" --restore-height=1
 }
 
 
