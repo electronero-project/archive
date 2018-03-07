6bdce5722f27d181260b88ad4f9094e07554f532
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-06 19:17:43 -0500

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -91,11 +91,11 @@ static const struct {
   time_t time;
 } mainnet_hard_forks[] = {
   // version 1 from the start of the blockchain
-  { 1, 0, 0, 1513821256 },
+  { 1, 1, 0, 1513821256 },
   // versions 6+ ring signatures are required, minimum 5 RCT enforced from here on.
-  { 6, 1, 0, 1518925393 },
+  { 6, 3, 0, 1518925393 },
 };
-static const uint64_t mainnet_hard_fork_version_1_till = (uint64_t)-1;
+static const uint64_t mainnet_hard_fork_version_1_till = 2;
 
 static const struct {
   uint8_t version;
