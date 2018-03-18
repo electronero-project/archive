82b82c7fc5972995481479fdaa8aac4bf8ed8159
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-18 05:14:12 -0400

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -171,7 +171,7 @@ namespace config
     uint16_t const RPC_DEFAULT_PORT = 21081;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 24082;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0xA1,0x61, 0x17, 0x31, 0xA1, 0x11, 0x04 , 0x41 0x00, 0x82, 0x16
+        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0xA1,0x61, 0x17, 0x31, 0xA1, 0x11, 0x04, 0x41, 0x00, 0x82, 0x16
       } }; // Bender's daydream
     std::string const GENESIS_TX = "010a01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121016ca5702486c9553884dabcbe7557d9b18357c6f0cc52e5cf675409df15cfb9cf";
     uint32_t const GENESIS_NONCE = 10001;
