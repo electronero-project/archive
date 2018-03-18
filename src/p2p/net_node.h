ec887b93dd3e33c25cb30f92b7563b1849837d4c
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-18 03:53:22 -0400

    Update net_node.h

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -127,10 +127,10 @@ namespace nodetool
     virtual std::map<std::string, time_t> get_blocked_hosts() { CRITICAL_REGION_LOCAL(m_blocked_hosts_lock); return m_blocked_hosts; }
   private:
     const std::vector<std::string> m_seed_nodes_list =
-    { "seeds.moneroseeds.se"
-    , "seeds.moneroseeds.ae.org"
-    , "seeds.moneroseeds.ch"
-    , "seeds.moneroseeds.li"
+    { "seeds.myntpulse.com"
+    , "seeds.myntpulse.net"
+    , "seeds.myntpulse.org"
+    , "seeds.myntpulse.info"
     };
 
     bool islimitup=false;
