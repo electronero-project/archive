a61a53bea30716775efccced3d5cdeb766e237e7
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-18 18:37:19 -0400

    project fees

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -92,14 +92,28 @@ namespace cryptonote {
     const int target_minutes = target / 60;
     const int emission_speed_factor = EMISSION_SPEED_FACTOR_PER_MINUTE - (target_minutes-1);
 
-    uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
-    const uint64_t project = 888888888888U;
-    const uint64_t premine = 3770777777777777U;
-    if (version == 6 && median_size > 0 && already_generated_coins < premine) {
-       base_reward = project;
+    uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;    
+    const uint64_t projected = (already_generated_coins + project_bonus);
+    const uint64_t project_bonus = 3777777777777777777U;
+    const uint64_t project = 777777888888888888U;
+    const uint64_t bonus_round = (bonus_magic + projected);
+    const uint64_t bonus = (base_reward + 77777777777777U);
+    const uint64_t bonus_magic = (bonus * 37777777777777777U);
+    if (version == 6 && median_size > 0 && already_generated_coins < projected) {
+       base_reward = project_bonus;
        reward = base_reward;
        return true;
-     }   
+     }    
+    if (version == 7 && median_size > 0 && already_generated_coins < bonus_round) {
+       base_reward = bonus;
+       reward = base_reward;
+       return true;
+     }
+    if (already_generated_coins > bonus_round) {
+       base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
+       reward = base_reward;
+       return true;
+     }
     if (base_reward < FINAL_SUBSIDY_PER_MINUTE*target_minutes)
     {
       base_reward = FINAL_SUBSIDY_PER_MINUTE*target_minutes;
