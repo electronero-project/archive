da511d05c4deb247f7dcae82e1de314dea5355da
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-18 21:19:22 -0400

    testnet

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -164,16 +164,16 @@ namespace config
 
   namespace testnet
   {
-    uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 46010;
-    uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 54;
-    uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 63;
+    uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 110;
+    uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 19;
+    uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 19;
     uint16_t const P2P_DEFAULT_PORT = 21080;
     uint16_t const RPC_DEFAULT_PORT = 21081;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 24082;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0xA1,0x61, 0x17, 0x31, 0xA1, 0x11, 0x04, 0x41, 0x00, 0x82, 0x16
+        0x04, 0xA4, 0xEA, 0x5D, 0xD1, 0x2C, 0xF8, 0x23, 0xE1, 0x66, 0xC2, 0xE3, 0x85, 0x8E, 0xC8, 0x39
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "010a01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121016ca5702486c9553884dabcbe7557d9b18357c6f0cc52e5cf675409df15cfb9cf";
+    std::string const GENESIS_TX = "010a01ff0001e9fe91b5ba02029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101e651939cf6c5af2020a037fac3675acd58d9ab2ab3e6a1664ca169670eddd30e";
     uint32_t const GENESIS_NONCE = 10001;
   }
 
