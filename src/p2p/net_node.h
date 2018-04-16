c3b0794994e5bca4925d8d1ac83c321e90210c05
Interchained <president@worldvaporexpo.com>
2018-04-15 21:08:26 -0400

    Update net_node.h

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -131,10 +131,10 @@ namespace nodetool
     virtual std::map<std::string, time_t> get_blocked_hosts() { CRITICAL_REGION_LOCAL(m_blocked_hosts_lock); return m_blocked_hosts; }
   private:
     const std::vector<std::string> m_seed_nodes_list =
-    { "seed1.kryptoniumpulse.com"
-    , "seed2.kryptoniumpulse.com"
-    , "seed3.kryptoniumpulse.com"
-    , "seed4.kryptoniumpulse.com"
+    { "seeds.electroneropulse.com"
+    , "seeds.electroneropulse.net"
+    , "seeds.electroneropulse.org"
+    , "seeds.electroneropulse.info"
     };
 
     bool islimitup=false;
