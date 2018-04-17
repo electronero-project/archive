3684ea35fb23b2e15bdb7af8929ddec1bda01319
Interchained <president@worldvaporexpo.com>
2018-04-16 20:22:23 -0400

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -763,7 +763,7 @@ difficulty_type Blockchain::get_difficulty_for_next_block()
     return (difficulty_type) 100;
   }
   // Reset network hashrate to 333.0 MHz when hardfork v2 comes
-  if ((uint64_t)height >= MAINNET_HARDFORK_V2_HEIGHT + 2 && (uint64_t)height <= MAINNET_HARDFORK_V2_HEIGHT + (uint64_t)difficulty_blocks_count){
+  if ((uint64_t)height >= MAINNET_HARDFORK_V2_HEIGHT + 1 && (uint64_t)height <= MAINNET_HARDFORK_V2_HEIGHT + (uint64_t)difficulty_blocks_count){
     return (difficulty_type) 19924656977;
   }
 
