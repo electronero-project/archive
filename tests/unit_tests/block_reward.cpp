4a97b2cbde19d7639ef1fafdcf230e2d1c8ac0aa
Interchained <president@worldvaporexpo.com>
2018-04-21 14:58:16 -0400

    Update block_reward.cpp

--- tests/unit_tests/block_reward.cpp
+++ tests/unit_tests/block_reward.cpp
@@ -47,7 +47,7 @@ namespace
   };
 
   #define TEST_ALREADY_GENERATED_COINS(already_generated_coins, expected_reward)                              \
-    m_block_not_too_big = get_block_reward(0, current_block_size, already_generated_coins, m_block_reward,1); \
+    m_block_not_too_big = get_block_reward(0, current_block_size, already_generated_coins, m_block_reward,1,0); \
     ASSERT_TRUE(m_block_not_too_big);                                                                         \
     ASSERT_EQ(m_block_reward, expected_reward);
 
