f20f2467d7176e9696fa35c201aeabd3dd599059
Interchained <president@worldvaporexpo.com>
2018-05-16 22:31:32 -0400

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -2610,7 +2610,7 @@ bool Blockchain::check_tx_inputs(transaction& tx, tx_verification_context &tvc, 
     size_t n_unmixable = 0, n_mixable = 0;
     size_t mixin = std::numeric_limits<size_t>::max();
     // const size_t min_mixin = hf_version >= HF_VERSION_MIN_MIXIN_6 ? 6 : hf_version >= HF_VERSION_MIN_MIXIN_4 ? 4 : 2;
-    const size_t min_mixin = DEFAULT_MIXIN;
+    const size_t min_mixin = MIN_MIXIN;
     const size_t max_mixin = MAX_MIXIN;
     for (const auto& txin : tx.vin)
     {
